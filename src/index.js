a11a00f1628f05dffd226bd617d36412c34fa11d
Tom <linecontroltran@gmail.com>
2023-09-04 14:57:27 -0400

    Version 6, Testing a new branch as well

--- src/index.js
+++ src/index.js
@@ -4,33 +4,48 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-function ImageComponent() {
-return (
-  <div>
-    <img src="logo192.png" width= "40px" />
-  </div>
-);
-  }
+
 
   function HeaderComponent () {
     return (
-    <div>
-      
-      <h1>A good ol Header for my Page.</h1>
-      
-    </div>
+    <header>
+      <nav className="nav">
+    
+       <img src="logo192.png" className="nav-logo" />
+     
+      <ul className="nav-items">
+        <li>Prices</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+     
+      </nav>
+      </header>
   );
+    }
+
+    function Footer () {
+      return (
+        <div>
+          <p className="all-rights">All Rights Reserved</p>
+        </div>
+      )
     }
 
     function ListComponent () {
       return (
       <div>
+         <h1>A good ol Header for my Page.</h1>
         <ul>
         <li>And some deets</li>
         <li>Some random junk here</li>
         <li>Some random poppycock here</li>
         <li>and finally last crap here</li>
         </ul>
+        <ol>
+        <li>Hi there how are you?</li>
+        <li>And thank you for visiting my page!</li>
+        </ol>
       
       </div>
     );
@@ -39,9 +54,9 @@ return (
       function Page() {
         return (
           <div>
-            <ImageComponent />
             <HeaderComponent />
             <ListComponent />
+            <Footer />
           </div>
         );
       }
