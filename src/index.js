3b6f7bf77a7ecbafcaa071ea9c172f9bfee8aa14
Tom <linecontroltran@gmail.com>
2023-09-03 23:53:32 -0400

    version 5, separating a function into multiple components, and put them into one component to render into the root element

--- src/index.js
+++ src/index.js
@@ -4,22 +4,49 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-function TestFunction () {
-  return (
+function ImageComponent() {
+return (
   <div>
     <img src="logo192.png" width= "40px" />
-    <h1>Testing react static page</h1>
-    <ul>
-    <li>Some random stuff here</li>
-    <li>Some random junk here</li>
-    <li>Some random poppycock here</li>
-    <li>and finally last crap here</li>
-    </ul>
-  
   </div>
-)
+);
   }
 
-ReactDOM.render(<TestFunction />, document.getElementById("root"))
+  function HeaderComponent () {
+    return (
+    <div>
+      
+      <h1>A good ol Header for my Page.</h1>
+      
+    </div>
+  );
+    }
+
+    function ListComponent () {
+      return (
+      <div>
+        <ul>
+        <li>And some deets</li>
+        <li>Some random junk here</li>
+        <li>Some random poppycock here</li>
+        <li>and finally last crap here</li>
+        </ul>
+      
+      </div>
+    );
+      }
+  
+      function Page() {
+        return (
+          <div>
+            <ImageComponent />
+            <HeaderComponent />
+            <ListComponent />
+          </div>
+        );
+      }
+
+ReactDOM.render(<Page />, document.getElementById("root"))
+
 
 //npm run start
